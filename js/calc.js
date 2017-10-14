00e9017dec08628fefab7128aeca3cad7c7b4e3d
Slawomir Bando <Slawomir Bando>
2017-10-14 23:41:08 +0200

    Added chart window

--- js/calc.js
+++ js/calc.js
@@ -46,14 +46,25 @@ function buttonClicked(){
         clearAll()
     } else if (clickedValue == "tg"){
         console.log("not finished")
+        showValueOnScreen("not done")
+        clearAll()
+        return
     } else if (clickedValue == "ctg"){
         console.log("not finished")
+        showValueOnScreen("not done")
+        clearAll()
+        return
     } else if (clickedValue == "sin"){
         console.log("not finished")
+        showValueOnScreen("not done")
+        clearAll()
+        return    
     } else if (clickedValue == "cos"){
         console.log("not finished")
+        showValueOnScreen("not done")
+        clearAll()
+        return    
     }
-
 
     showValueOnScreen(calculation)
 }
@@ -77,3 +88,53 @@ function undoOneLetter(){
     }
 }
 
+function drawLines(){
+    let xLines = 16
+    let yLines = 8
+
+    let canvas =  document.getElementById("myCanvas")
+    let ctx = canvas.getContext("2d");
+
+    let width = canvas.width
+    let height = canvas.height
+
+    //write x and y lines
+    for(let i=0; i<yLines; i++){
+        ctx.moveTo(0,height/yLines*i);
+        ctx.lineTo(width,height/yLines*i);
+    }
+
+    for(let i=0; i<xLines; i++){
+        ctx.moveTo(width/xLines*i,0);
+        ctx.lineTo(width/xLines*i,height);
+    }
+
+    ctx.stroke();
+}
+
+/*
+function drawTg(){
+    let xLines = 16
+    let yLines = 8
+
+    let canvas =  document.getElementById("myCanvas")
+    let ctx = canvas.getContext("2d");
+
+    let width = canvas.width
+    let height = canvas.height
+
+    let xPxPer1cm = width / xLines;
+    let yPxPer1cm = height / yLines;
+
+    ctx.beginPath()
+
+    for(let x=-(xLines/2)+0.01; x<=xLines/2; x=x+0.01){
+        ctx.moveTo((x-0.01+4) * xPxPer1cm, height/2 - Math.tan(x-0.01)*height )
+        ctx.lineTo((x+4) * xPxPer1cm,height/2 - Math.tan(x)*height)
+    }
+    ctx.stroke();
+}
+*/
+
+drawLines()
+//drawTg()
